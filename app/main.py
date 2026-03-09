e7f258ebde5414ca1d3dfe0a69da40d3241d92fa
subeeen <rose9165676@naver.com>
2026-03-09 19:13:06 +0900

    [feat] FastAPI, API 라우터 정의

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from app.api.v1.api import api_router
+from app.db.database import Base, engine
+from app.models.user import User
 
 app = FastAPI(
     title="NewLearn API",
@@ -8,3 +11,9 @@ app = FastAPI(
 @app.get("/")
 def root():
     return {"message": "server running"}
+
+# DB 테이블 생성
+Base.metadata.create_all(bind=engine)
+
+# 라우터 연결
+app.include_router(api_router, prefix="/api/v1")
