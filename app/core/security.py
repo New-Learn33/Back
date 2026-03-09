3f5b0b3ca77601462361da59cf05659b16abffb9
subeeen <rose9165676@naver.com>
2026-03-09 19:15:00 +0900

    [feat] JWT utility 추가, Google ID 토큰 인증 서비스 추가

---
+++ app/core/security.py
@@ -0,0 +1,23 @@
+from jose import jwt
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("JWT_SECRET")
+# JWT 서명 알고리즘
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_DAYS = 7
+
+# JWT 만드는 함수
+def create_access_token(data: dict):
+    
+    if not SECRET_KEY:
+        raise ValueError("JWT_SECRET is not set")
+    
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(days=ACCESS_TOKEN_EXPIRE_DAYS)
+    to_encode.update({"exp": expire})
+    # payload를 SECRET_KEY로 서명해서 문자열 토큰으로 변환
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
