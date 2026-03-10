2d80d23046c31cb97fd5c1d2bf86116fb2a9c5e8
ljo1010 <ljo111004@gmail.com>
2026-03-11 00:18:07 +0900

    [fix]시큐리티 부분에 해싱되게 고침 + requirements에 라이브러리 추가

--- app/core/security.py
+++ app/core/security.py
@@ -1,6 +1,6 @@
 from jose import jwt, JWTError
 from datetime import datetime, timedelta
-from passlib.context import CryptContext
+import bcrypt
 from dotenv import load_dotenv
 import os
 
@@ -12,13 +12,17 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_DAYS = 7
 
 # 비밀번호 해싱
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
 def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
+    password_bytes = password.encode("utf-8")
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password_bytes, salt)
+    return hashed.decode("utf-8")
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
+    return bcrypt.checkpw(
+        plain_password.encode("utf-8"),
+        hashed_password.encode("utf-8")
+    )
 
 # JWT 만드는 함수
 def create_access_token(data: dict):
